315a9a0f4b836d61c96a537db8a9608c9adafcbb
Andrea Zonca <code@andreazonca.com>
2011-06-29 12:21:41 -0700

    improved read map doc

--- pycfitsio/healpix.py
+++ pycfitsio/healpix.py
@@ -4,7 +4,9 @@ def read_map(filename, columns=None):
     """Read healpix map and return IQU or I array and header dictionary
     
     columns: None reads all the columns, otherwise a list of integers zero-based column numbers
-            or a list of column names"""
+            or a list of column names
+            
+    Returns the data array and a header dictionary"""
 
     #TODO context manager
     f = open(filename)
