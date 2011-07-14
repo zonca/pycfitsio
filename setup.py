a41ab990366dfa82773c9780a7fa60ae2663d107
Andrea Zonca <code@andreazonca.com>
2011-07-14 17:53:58 +0200

    added distibute setup and gplv3 license

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name = "pycfitsio",
+    version = "0.1.0",
+
+    packages=['pycfitsio', 'pycfitsio.test'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['docutils>=0.3'],
+    author = "Andrea Zonca",
+    author_email = "code@andreazonca.com",
+    description = "Simple FITS files I/O package",
+    license='LICENSE.txt',
+    keywords = "fits, astrophysics, pyfits",
+    url = "http://github.com/zonca/pycfitsio",
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Physics',
+          ],
+    long_description=open('README.rst').read(),
+
+)
