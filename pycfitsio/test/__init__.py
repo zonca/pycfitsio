7ee8a1e7b5c0a7d27056087f0886af8434ad7881
Andrea Zonca <code@andreazonca.com>
2011-07-15 18:06:24 +0200

    test case for cfitsio error

--- pycfitsio/test/__init__.py
+++ pycfitsio/test/__init__.py
@@ -3,5 +3,13 @@ import unittest
 from read import *
 from write import *
 
+class TestError(unittest.TestCase):
+
+    def test_error(self):
+        
+        self.assertRaises(CfitsioError, open, 'unexisting_file.fits')
+        self.assertRaisesRegexp(CfitsioError, 'could not open',
+                        open, 'unexisting_file.fits')
+
 if __name__ == '__main__':
     unittest.main()
